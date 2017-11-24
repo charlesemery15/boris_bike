38e477b6ff28726aae91271ef502070f3786371c
charles <charlesemery15@gmail.com>
2017-11-24 15:39:23 +0000

    Step_19 Double Broken

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -9,27 +9,28 @@ describe DockingStation do
     #   is_expected.to respond_to :release_bike #whenever refereing to a method inside rspec use symbol eg//: :release_bike not release_bike
     # end
   it "releases working bikes" do
-    bike = Bike.new
-    subject.dock(bike)
+    #bike = Bike.new
+    subject.dock(double(:bike))
     bike = subject.release_bike # setting up test ^
     expect(bike).to be_working # test working? (method) is returned true | to_not be_working return false
   end
-      it "dock has accepted an argument" do
-        expect(subject).to respond_to(:dock).with(1).argument
-      end
 
-      it 'has a default capacity' do
-        expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
-      end
+  it "dock has accepted an argument" do
+    expect(subject).to respond_to(:dock).with(1).argument
+  end
 
-      it "allows user to set capacity of docking station" do
-        expect(subject.capacity = 50).to eq(50)
-      end
+  it 'has a default capacity' do
+    expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
+  end
+
+  it "allows user to set capacity of docking station" do
+    expect(subject.capacity = 50).to eq(50)
+  end
 
   describe "#release_bike" do
 
     it "releases a bike" do
-      a_bike = Bike.new
+      a_bike = double(:bike)
       subject.dock(a_bike)
       expect(subject.release_bike).to eq a_bike  #has () as testing if eq
     end
@@ -39,7 +40,7 @@ describe DockingStation do
     end
 
     it "can't release a broken bike" do
-      a_bike = Bike.new
+      a_bike = double(:bike)
       a_bike.report_broken
       subject.dock(a_bike)
       expect{ subject.release_bike }.to raise_error("Cannot release a broken bike")
@@ -51,17 +52,17 @@ describe DockingStation do
   describe "#dock" do
 
     it "dock a bike" do
-      a_bike = Bike.new
+      a_bike = double(:bike)
       expect(subject.dock(a_bike)).to include a_bike
     end
 
     it "should raise error when full of bikes" do
-      subject.capacity.times { subject.dock(Bike.new) }
-      expect{ subject.dock(Bike.new) }.to raise_error(RuntimeError, "Dock full")
+      subject.capacity.times { subject.dock(double(:bike)) }
+      expect{ subject.dock(double(:bike)) }.to raise_error(RuntimeError, "Dock full")
     end
 
     it "accepts a broken bike" do
-      a_bike = Bike.new
+      a_bike = double(:bike)
       a_bike.report_broken
       expect(subject.dock(a_bike)).to include a_bike
 
